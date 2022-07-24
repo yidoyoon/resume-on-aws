4263f6ddc33c32a205b9e5e3cae40c5e449da2fd
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-07-24 19:46:02 +0900

    style: fix quotes pair
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

--- assets/web/view_count.js
+++ assets/web/view_count.js
@@ -1,8 +1,8 @@
-let url = new URL('https://t0j7qbuiu8.execute-api.ap-northeast-2.amazonaws.com/prod/resume-on-aws-UpdateCounter-8XiUSbUelfWW');
+let url = new URL("https://t0j7qbuiu8.execute-api.ap-northeast-2.amazonaws.com/prod/resume-on-aws-UpdateCounter-8XiUSbUelfWW");
 
 async function updateCount() {
     let response = await fetch(url);
-    document.getElementById('count').innerHTML = await response.text();
+    document.getElementById("count").innerHTML = await response.text();
 }
 
 (async() => {
