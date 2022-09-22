c0741f386c66046d811a1f2ce8f75f8b7e394775
Yi Byeongyong <ybyo@yibyeongyong.com>
2022-09-22 22:18:25 +0900

    chore: reformat code
    Signed-off-by: Yi Byeongyong <ybyo@yibyeongyong.com>

--- assets/web/en/view_count.js
+++ assets/web/en/view_count.js
@@ -1,20 +1,22 @@
-let url = new URL("https://7a0xc4z2ud.execute-api.us-east-1.amazonaws.com/prod/updateViews");
+let url = new URL(
+  "https://7a0xc4z2ud.execute-api.us-east-1.amazonaws.com/prod/updateViews"
+);
 
 async function updateCount(url) {
-    fetch(url)
-        .then(async (response) => {
-            let data = await response.json()
-            if (response.status === 200) {
-                document.getElementById("views").innerHTML = data.body
-            } else {
-                console.log("Something went wrong. HTTP response status not 200");
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+  fetch(url)
+    .then(async (response) => {
+      let data = await response.json();
+      if (response.status === 200) {
+        document.getElementById("views").innerHTML = data.body;
+      } else {
+        console.log("Something went wrong. HTTP response status not 200");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 (async () => {
-    await updateCount(url);
+  await updateCount(url);
 })();
--- assets/web/ko/view_count.js
+++ assets/web/ko/view_count.js
@@ -1,20 +1,22 @@
-let url = new URL("https://t0j7qbuiu8.execute-api.ap-northeast-2.amazonaws.com/prod/resume-on-aws-UpdateCounter-8XiUSbUelfWW");
+let url = new URL(
+  "https://t0j7qbuiu8.execute-api.ap-northeast-2.amazonaws.com/prod/resume-on-aws-UpdateCounter-8XiUSbUelfWW"
+);
 
 async function updateCount(url) {
-    fetch(url)
-        .then(async (response) => {
-            let data = await response.json()
-            if (response.status === 200) {
-                document.getElementById("views").innerHTML = data.body
-            } else {
-                console.log("Something went wrong. HTTP response status not 200");
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+  fetch(url)
+    .then(async (response) => {
+      let data = await response.json();
+      if (response.status === 200) {
+        document.getElementById("views").innerHTML = data.body;
+      } else {
+        console.log("Something went wrong. HTTP response status not 200");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 (async () => {
-    await updateCount(url);
+  await updateCount(url);
 })();
